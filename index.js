1a29e3238c59934902a0eb184a49d79dfecb2a9b
Burt <1020103647@qq.com>
2022-11-09 10:37:13 +0800

    feat: express cors 安装，并编写最基本代码

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+  res.send("Hello /, I'm Express server for SSE!");
+});
+
+app.get('/stream', async (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive',
+    'Access-Control-Allow-Origin': '*',
+  });
+  // console.log(scheduler.queue)
+  // res.write('retry: 10000\n');
+  // res.write('event: connecttime\n');
+  // res.write('data: ' + 1000 + '\n\n')
+  // res.write('data: ' + new Date() + '\n\n');
+  const mockData = JSON.stringify({
+    name: 'lucy',
+    age: 18,
+  });
+  res.write('data: ' + mockData + '\n\n');
+
+  const interval = setInterval(function () {
+    res.write('data: ' + mockData + '\n\n');
+  }, 1000);
+});
+
+app.listen(44000, () => {
+  console.log(`Example app listening on port 44000`);
+});
